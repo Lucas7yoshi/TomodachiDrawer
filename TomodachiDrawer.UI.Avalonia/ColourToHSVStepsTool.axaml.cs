3bb06ae9b758c0040fdbded08b0e371592749219
Martín (Netux) Rodriguez <send.email@netux.site>
2026-05-16 19:43:31 -0300

    Improve Colour to HSV Steps UI
    - Use controller inputs names
    - Reorder fields to match HSV
    - Use Saturation and Value as labels for X and Y

--- TomodachiDrawer.UI.Avalonia/ColourToHSVStepsTool.axaml.cs
+++ TomodachiDrawer.UI.Avalonia/ColourToHSVStepsTool.axaml.cs
@@ -21,36 +21,34 @@ public partial class ColourToHSVStepsTool : Window
         if (steps.HueSteps > ColourGameSteps.FCR_HUE_SLIDER_STEP_COUNT / 2)
         {
             HueStepsOutput.Text = ((ColourGameSteps.FCR_HUE_SLIDER_STEP_COUNT - 1) - steps.HueSteps).ToString();
-            HueStepsOutput.InnerRightContent = "taps from the right";
+            HueStepsOutput.InnerRightContent = "ZR taps";
         }
         else
         {
             HueStepsOutput.Text = steps.HueSteps.ToString();
-            HueStepsOutput.InnerRightContent = "taps from the left";
+            HueStepsOutput.InnerRightContent = "ZL taps";
         }
 
         if (steps.SatSteps > ColourGameSteps.FCR_SATURATION_STEP_COUNT / 2)
         {
             SatStepsOutput.Text = ((ColourGameSteps.FCR_SATURATION_STEP_COUNT - 1) - steps.SatSteps).ToString();
-            SatStepsLabel.Text = "Left:";
+            SatStepsOutput.InnerRightContent = "right taps";
         }
         else
         {
             SatStepsOutput.Text = steps.SatSteps.ToString();
-            SatStepsLabel.Text = "Right:";
+            SatStepsOutput.InnerRightContent = "left taps";
         }
-        SatStepsOutput.InnerRightContent = "taps";
 
         if (steps.ValSteps > ColourGameSteps.FCR_VALUE_STEP_COUNT / 2)
         {
             ValStepsOutput.Text = ((ColourGameSteps.FCR_VALUE_STEP_COUNT - 1) - steps.ValSteps).ToString();
-            ValStepsLabel.Text = "Bottom:";
+            ValStepsOutput.InnerRightContent = "up taps";
         }
         else
         {
             ValStepsOutput.Text = steps.ValSteps.ToString();
-            ValStepsLabel.Text = "Top:";
+            ValStepsOutput.InnerRightContent = "down taps";
         }
-        ValStepsOutput.InnerRightContent = "taps";
     }
 }
